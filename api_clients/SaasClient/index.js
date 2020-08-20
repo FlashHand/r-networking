a307f6d56a2869ffd7969b34ca20b30427d04aae
rys-32 <rys-32@rys-32deMacBook-Pro.local>
2020-08-20 10:48:37 +0800

    add mw in mw

--- api_clients/SaasClient/index.js
+++ api_clients/SaasClient/index.js
@@ -4,9 +4,12 @@
  * @date 2020/8/7 11:46 AM
  */
 const BasicClient = require('../BasicClient');
-
+const req_pre_mw = require('./req_pre_mw');
+const res_sub_mw = require('./res_sub_mw');
 class SaasClient extends BasicClient {
   constructor(config = {}, request_mws = [], response_mws = []) {
+    request_mws.unshift(req_pre_mw);
+    response_mws.push(res_sub_mw);
     super(config, request_mws, response_mws)
   }
 }
