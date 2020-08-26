cdb3736e7c31c0ff115a4117055aaf0013dbdac6
rys-32 <>
2020-08-26 19:14:55 +0800

    nm

--- api_clients/MWSlotsManager.js
+++ api_clients/MWSlotsManager.js
@@ -4,32 +4,30 @@
  * @date 2020/8/26 5:24 PM
  */
 class MWClass {
-  mws = [];
-  fullfilled = (config) => {
-    let finalConfig = config;
-    for (let mw of this.mws){
-      finalConfig = mw.fullfilled(config);
-    }
-    return finalConfig;
-  };
-  rejected = (error) => {
-    return Promise.reject(error);
-  };
+  constructor() {
+    this.mws = [];
+    this.fullfilled = (config) => {
+      let finalConfig = config;
+      for (let mw of this.mws){
+        finalConfig = mw.fullfilled(config);
+      }
+      return finalConfig;
+    };
+    this.rejected = (error) => {
+      return Promise.reject(error);
+    };
+  }
   set(injectedMWs){
     this.mws = injectedMWs;
-  }
-  constructor() {
-
   }
 }
 
 class MWSlotsManager {
-  req_pre = new MWClass();
-  res_pre = new MWClass();
-  req_sub = new MWClass();
-  res_sub = new MWClass();
   constructor() {
-
+    this.req_pre = new MWClass();
+    this.res_pre = new MWClass();
+    this.req_sub = new MWClass();
+    this.res_sub = new MWClass();
   }
 
 }
