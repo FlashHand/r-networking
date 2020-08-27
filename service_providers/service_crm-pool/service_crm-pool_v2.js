54ffb6f4f5edc627e7e06c3bdd8b16f49ef96c14
冯丹凤 <937326389@qq.com>
2020-08-27 21:31:27 +0800

    变更注释

--- service_providers/service_crm-pool/service_crm-pool_v2.js
+++ service_providers/service_crm-pool/service_crm-pool_v2.js
@@ -2,8 +2,8 @@
  * @Author: 冯丹凤
  * @Date: 2020-08-25 11:20:27
  * @LastEditors: 冯丹凤
- * @LastEditTime: 2020-08-25 17:40:58
- * @Description: 原 /rys-crm-pool 转成 /crm-customer
+ * @LastEditTime: 2020-08-26 20:33:05
+ * @Description: 原 /rys-crm-pool 转成 /crm-pool
  */
 
 const saas = require('../../networking_utils/saas');
