d892c81afa8bfdbd78d844a8d3a5f0fa41dfa831
张赛辉 <1013812326@qq.com>
2020-08-28 19:32:26 +0800

    /product-center/v2

--- service_providers/service_product-center/service_product-center_v2.js
+++ service_providers/service_product-center/service_product-center_v2.js
@@ -1,7 +1,7 @@
-const server = require("../../networking_utils/saas")
-const res_err_msg = require("../../middlewares/saas/response/res_err_msg")
+const server = require("../../networking_utils/saas");
+const res_err_msg = require("../../middlewares/saas/response/res_err_msg");
 /**
  * /product-center/api/v1/ -> /product-center/v2/
  * /api/rys/ -> /product-center/v2/
  */
-module.exports = server.generateClients("/product-center/v2",[] ,[ ]);
+module.exports = server.generateClients("/product-center/v2", [], [res_err_msg]);
