130723a96205e1af62fcf6459e2e8bac12e44356
rys-32 <>
2020-09-05 09:55:14 +0800

    nm

--- service_providers/service_serverless/service_serverless_sap.js
+++
@@ -1,9 +0,0 @@
-/**
- * 赛辉
- * @authors Your Name (you@example.org)
- * @date    2020-08-28 16:57:37
- * @version 1.0.0
- */
-
-const saas = require("../../networking_utils/saas");
-module.exports = saas.generateClients("/serverless/v2/api/sap");
