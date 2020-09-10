f8363e7c23aeb6d2587039d8a2b8d55fdc3cfbc1
rys-32 <>
2020-09-10 15:28:22 +0800

    add rys-coupon_v2

---
+++ service_providers/service_rys-coupon/service_rys-coupon_v2.js
@@ -0,0 +1,8 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/9/10 3:25 PM
+ */
+const server = require("../../networking_utils/saas");
+module.exports = server.generateClients("/rys-coupon/v2");
