f149e4d2ab6631119426a11edc7557e7a18274a0
rys-32 <>
2020-09-18 11:32:29 +0800

    add tax-console

---
+++ service_providers/service_tax-console /service_tax-console.js
@@ -0,0 +1,8 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/9/18 11:31 AM
+ */
+const server = require("../../networking_utils/saas")
+module.exports = server.generateClients("/tax-console")
