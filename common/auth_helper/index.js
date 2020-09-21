9d754bff848a565acb18ae87031610f7348db117
rys-32 <>
2020-09-21 17:22:57 +0800

    nm

---
+++ common/auth_helper/index.js
@@ -0,0 +1,6 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/9/19 1:30 PM
+ */
--- middlewares/saas/response/res_handler.js
+++ middlewares/saas/response/res_handler.js
@@ -3,16 +3,16 @@
  * @author Wang Bo (ralwayne@163.com)
  * @date 2020/8/7 11:10 AM
  */
-const {service,window_helper} = require('r-foundation');
+const {app_helper} = require('r-foundation');
 const fullfilled = response => {
   try {
     //data 是undefined时设为空对象
-    if (response.data.data === undefined)response.data.data = {};
+    if (response.data.data === undefined) response.data.data = {};
     response.data.code = parseInt(response.data.code);
     //登录失效鉴权
     let serviceCode = response.data.serviceCode;
-    if (service.isTokenInvalid(serviceCode)) {
-      window_helper.goLogin()
+    if (app_helper.isTokenInvalid(serviceCode)) {
+      app_helper.goLogin()
     }
   } catch (e) {
     console.log(e);
@@ -23,7 +23,7 @@ const rejected = error => {
   return Promise.reject(error)
 }
 module.exports = {
-  name:'res_handler',
+  name: 'res_handler',
   fullfilled,
   rejected
 }
