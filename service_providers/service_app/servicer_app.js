979e6e01399f2ee3b8387869f668a95d11979041
rys-32 <>
2020-09-28 15:00:24 +0800

    add service_app

---
+++ service_providers/service_app/servicer_app.js
@@ -0,0 +1,8 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/9/28 2:59 PM
+ */
+const saas = require('../../networking_utils/saas');
+module.exports = saas.generateClients('/app');
