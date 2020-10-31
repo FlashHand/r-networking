e19a1463929fb7d63010c46c43d6d6fde4b961b7
wangbo <>
2020-10-31 11:37:05 +0800

    add queqiao-platform

---
+++ service_providers/service_queqiao-platform/service_queqiao-platform.js
@@ -0,0 +1,11 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/10/31 11:33 AM
+ */
+const server = require("../../networking_utils/saas")
+/**
+ * 老接口/api/v1/crm/
+ */
+module.exports = server.generateClients("/queqiao-platform");
