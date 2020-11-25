f4c4cf728f564b0827c17d4094f814a6b43096e1
wangbo <>
2020-11-25 10:59:32 +0800

    change service_user-center.js to rysclient

--- service_providers/service_user-center/service_user-center.js
+++ service_providers/service_user-center/service_user-center.js
@@ -1,2 +1,4 @@
-const saas = require('../../networking_utils/saas');
-module.exports = saas.generateClients('/user-center');
+const RysClient = require('../../api_clients/RysClient');
+module.exports = RysClient.createClient('/user-center');
+// const saas = require('../../networking_utils/saas');
+// module.exports = saas.generateClients('/user-center');
